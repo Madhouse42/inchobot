c2cc2a138c26c6dca972acd98cd4bae7bc25cdf9
ream <1069700995@qq.com>
2013-11-17 13:12:01 +0800

    can input date now

--- ibot/inchobot.py
+++ ibot/inchobot.py
@@ -215,14 +215,24 @@ def addAssignment():
         return redirect('/')
 
     if request.method == 'GET':
-        return render_template('addAssignment.html', global_user=global_user, assignments=asses)
+        today = datetime.datetime.today()
+        msg = {'year': today.year, 'month': today.month, 'day': today.day, 'hour': today.hour, 'minute': today.minute}
+        return render_template('addAssignment.html', global_user=global_user, assignments=asses, msg=msg)
 
     # insert assignment into database here
     name = request.form.get('name')
     description = request.form.get('description')
     file_url = request.form.get('fileURL')
-    deadline = request.form.get('deadline')
-    deadline = datetime.datetime.today()  # TODO
+    try:
+        year = int(request.form.get('year'))
+        month =int(request.form.get('month'))
+        day = int(request.form.get('day'))
+        hour = int(request.form.get('hour'))
+        minute = int(request.form.get('minute'))
+        deadline = datetime.datetime(year, month, day, hour, minute)
+    except ValueError:
+        err = u'日期填写错误，请检查'
+        return render_template('addAssignment.html', global_user=global_user, assignments=asses, msg=request.form, err=err)
     new_ass = Assignment(name, global_user._id, file_url, description, deadline, datetime.datetime.today())
     db.session.add(new_ass)
     db.session.commit()
@@ -280,18 +290,30 @@ def update_assignment(_id):
     if ass.user != global_user:
         return redirect(url_for(home_page))
     if request.method == 'GET':
-        msg = {'description': ass.descriptions, 'name': ass.name, 'fileURL': ass.files_url, 'deadline': ass.deadline}
+        msg = {'description': ass.descriptions, 'name': ass.name, 'fileURL': ass.files_url,
+               'year': ass.deadline.year, 'day': ass.deadline.day, 'month': ass.deadline.month,
+               'hour': ass.deadline.hour, 'minute': ass.deadline.minute}
         return render_template('update_assignment.html', global_user=global_user, msg=msg)
     name = request.form.get('name')
     description = request.form.get('description')
     file_url = request.form.get('fileURL')
-    deadline = request.form.get('deadline')
-    deadline = datetime.datetime.today()  # TODO
+
+    try:
+        year = int(request.form.get('year'))
+        month =int(request.form.get('month'))
+        day = int(request.form.get('day'))
+        hour = int(request.form.get('hour'))
+        minute = int(request.form.get('minute'))
+        deadline = datetime.datetime(year, month, day, hour, minute)
+    except ValueError:
+        err = u'日期填写错误，请检查'
+        return render_template('update_assignment.html', global_user=global_user, msg=request.form, err=err)
+
     password = request.form.get('password')
     if password != global_user.password:
         err = u"密码输入错误"
         return render_template('update_assignment.html', global_user=global_user, err=err, msg=request.form)
-    #Assignment.query.filter(Assignment._id == _id).update({'name': name, 'descriptions': description, 'files_url': file_url, 'deadline': deadline})
+
     ass = Assignment.query.filter(Assignment._id == _id)
     ass.update({'name': name, 'descriptions': description, 'files_url': file_url, 'deadline': deadline})
 
