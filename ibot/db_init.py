a6cc2a290754fa58d71a48557e72bb500c36452a
ream <1069700995@qq.com>
2013-11-19 20:36:30 +0800

    fixed bug in db_init

--- ibot/db_init.py
+++ ibot/db_init.py
@@ -4,11 +4,13 @@ from sqlalchemy.exc import IntegrityError
 
 from ibot import db, User, Assignment, Discussion
 
+
 def run():
     db.create_all()
-    kami = User('0', 'kami', '-', '-', datetime.datetime.today(), 0)
-    db.session.add(kami)
-    db.session.commit()
+    if not User.query.filter(User._id == 1).first():
+        kami = User('-', 'kami', '-', '-', datetime.datetime.today(), 0)
+        db.session.add(kami)
+        db.session.commit()
 
 if __name__ == '__main__':
     run()
